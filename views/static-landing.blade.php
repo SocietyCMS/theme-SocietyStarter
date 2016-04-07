0ea5ca073c409d0bd2ceadea26639e6fc2c4da40
Ralph Huwiler <ralph@huwiler.rocks>
2016-04-07 23:27:01 +0200

    use social menu

--- views/static-landing.blade.php
+++ views/static-landing.blade.php
@@ -40,11 +40,7 @@
             </div>
             <div class="sidebar-module">
                 <h4>Elsewhere</h4>
-                <ol class="list-unstyled">
-                    <li><a href="#">GitHub</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Facebook</a></li>
-                </ol>
+                {!! Menu::get('social')->asOl(['class' => 'list-unstyled']) !!}
             </div>
         </div>
 
