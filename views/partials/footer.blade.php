3eec2c6a4534fd971c9a2ccfe079becc0869a27e
Ralph Huwiler <ralph@huwiler.rocks>
2016-04-08 18:24:21 +0200

    auth in footer

--- views/partials/footer.blade.php
+++ views/partials/footer.blade.php
@@ -1,6 +1,4 @@
 <footer class="site-footer">
     <p>Powered by <a href="https://societycms.com" target="_blank">SocietyCMS</a></p>
-    <p>
-        <a href="#">Back to top</a>
-    </p>
+    {!! Menu::get('auth')->asDiv() !!}
 </footer>
