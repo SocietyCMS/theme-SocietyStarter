4947c71d8beb920193d36c20e0a2fcb5a8a825e1
Ralph Huwiler <ralph@huwiler.rocks>
2016-04-14 16:00:58 +0200

    check if menu exists

--- views/partials/footer.blade.php
+++ views/partials/footer.blade.php
@@ -1,4 +1,6 @@
 <footer class="site-footer">
     <p>Powered by <a href="https://societycms.com" target="_blank">SocietyCMS</a></p>
-    {!! Menu::get('auth')->asDiv() !!}
+    @if(Menu::get('auth'))
+        {!! Menu::get('auth')->asDiv() !!}
+    @endif
 </footer>
